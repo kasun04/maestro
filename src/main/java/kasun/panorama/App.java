13095a0e9f68b9a3795055125ef3e0cf985f4de8
Kasun Indrasiri <kasun147@gmail.com>
2016-02-13 20:10:20 +0530

    Refactoring content

---
+++ src/main/java/kasun/panorama/App.java
@@ -0,0 +1,13 @@
+package kasun.panorama;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
