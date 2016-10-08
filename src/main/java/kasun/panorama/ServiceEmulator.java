d9127fdde4ac8ec45e17dad391ded527058be460
Kasun Indrasiri <kasun147@gmail.com>
2016-10-08 11:17:17 -0700

    Fixing sample configs

--- src/main/java/kasun/panorama/ServiceEmulator.java
+++ src/main/java/kasun/panorama/ServiceEmulator.java
@@ -1,21 +1,6 @@
 /*
  * *
- *  * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *  *
- *  * WSO2 Inc. licenses this file to you under the Apache License,
- *  * Version 2.0 (the "License"); you may not use this file except
- *  * in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  * http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing,
- *  * software distributed under the License is distributed on an
- *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  * KIND, either express or implied.  See the License for the
- *  * specific language governing permissions and limitations
- *  * under the License.
- *
+ *  * Copyright (C) Kasun Indrasiri - All Rights Reserved.
  */
 package kasun.panorama;
 
@@ -142,6 +127,8 @@ public class ServiceEmulator {
 
 
     public static void runServices() {
+
+        System.out.println("Working Dir : " + System.getProperty("user.dir"));
         Emulator.getHttpEmulator()
                 .server()
                 .given(configure().host("127.0.0.1").port(6060).context("/services"))
@@ -209,7 +196,7 @@ public class ServiceEmulator {
                 .when(request()
                               .withPath("GeoLocationToPostalCodeService")
                               .withMethod(HttpMethod.POST)
-                              .withHeader("Content-Type", "application/json"))
+                              /*.withHeader("Content-Type", "application/json")*/)
                 .then(response()
                               .withBody(createPayload("src/main/resources/ch_10/resources/sample_resources/message_formats/GeoLocationToPostalCode_res.json"))
                               .withStatusCode(HttpResponseStatus.OK)
